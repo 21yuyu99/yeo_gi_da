3bd8df3bc61184f46cc0075911dd08b9564101e4
21yuyu99 <yujeong0720@gmail.com>
2022-11-01 20:07:00 +0900

    BE,FE 개발환경

---
+++ FE-yeo_gi_da/components/layout/Header.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Header = () => {
+  return (
+    // 가로로 정렬하기 
+    <Head> 
+      <Record>
+
+      </Record>
+      <Logo>
+
+      </Logo>
+      <Login>
+
+      </Login>
+    </Head>
+  )
+}
+export const Head = styled.header`
+`
+
+export const Record = styled.span`
+`
+
+export const Logo = styled.span`
+`
+
+export const Login = styled.span`
+`
